e4b438e862121dd7a77cccfbcf053ae813ef542e
Partik Bolan <bolanpartik@gmail.com>
2024-10-07 15:29:13 +0530

    create auth middleware to verify jwtToken

--- JWT_authentication/index.js
+++ JWT_authentication/index.js
@@ -6,6 +6,25 @@ let users = []
 
 app.use(express.json())
 
+function auth(req, res, next) {
+    const token = req.headers.token;
+    if(token){
+        const verifyToken = jwt.verify(token, JWT_SECRET)
+        if (verifyToken.username) {
+            req.username = verifyToken.username
+            next()
+        } else {
+            res.send({
+                message: 'You are not signed in'
+            })
+        }
+    }else{
+        res.status(401).send({
+            message: 'Token missing'
+        })
+    }
+}
+
 app.post('/signup', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
@@ -18,21 +37,20 @@ app.post('/signup', (req, res) => {
     res.send({
         message: 'Signup done'
     })
-
 })
 
 app.post('/signin', (req, res) => {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = users.find(user => user.username == username && user.password == password)
+    const foundUser = users.find(user => user.username === username && user.password === password)
 
-    if (user) {
-        const jwtToken = jwt.sign({
+    if (foundUser) {
+        const token = jwt.sign({
             username: username
         }, JWT_SECRET)
         res.send({
-            jwtToken
+            token: token
         })
     } else {
         res.send({
@@ -41,29 +59,14 @@ app.post('/signin', (req, res) => {
     }
 })
 
-app.get('/me', (req, res) => {
-    const token = req.headers.authorization
+app.get('/me', auth, (req, res) => {
 
-    if (token) {
-        const userDetails = jwt.verify(token, JWT_SECRET)
-        const username = userDetails.username
-        const user = users.find(user => user.username == username)
+    const foundUser = users.find(user => user.username === req.username)
 
-        if (user) {
-            res.send({
-                username: user.username,
-                password: user.password
-            })
-        } else {
-            res.send({
-                message: 'Invalid token'
-            })
-        }
-    } else {
-        res.send({
-            message: 'JWT must be provided'
-        })
-    }
+    res.send({
+        username: foundUser.username,
+        password: foundUser.password
+    })
 })
 
 app.listen(3000)
