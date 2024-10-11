f4d3daedea68ed87aa6cc524fec89c718e38b504
Partik Bolan <bolanpartik@gmail.com>
2024-10-11 20:06:59 +0530

    completed signup endpoint and store user detail

--- Auth-todo-app/index.js
+++ Auth-todo-app/index.js
@@ -6,12 +6,40 @@ const JWT_SECRET = 'jwtsecrethere'
 const users = []
 app.use(express.json())
 
+function genUserId() {
+    const options = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+    let id = ''
+    for (let i = 0; i < 21; i++) {
+        id += options[Math.floor(Math.random() * options.length)]
+    }
+    return id;
+}
+
 function authentication(req, res, next) {
     // authentication using JWT
 }
 
 app.post('/signup', (req, res) => {
     // signup user with email pass password
+    const username = req.body.username;
+    const password = req.body.password;
+
+    if (!username || !password) {
+        return res.status(400).send({
+            message: 'Username and password must be provided'
+        })
+    }
+
+    const id = genUserId()
+    users.push({
+        username: username,
+        password: password,
+        userId: id,
+        todos: []
+    })
+    res.send({
+        message: 'Sign up done'
+    })
 
 })
 
