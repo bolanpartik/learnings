d7faeeda6eed69f2bae41972f2350fe7292d7329
Partik Bolan <bolanpartik@gmail.com>
2024-10-12 18:22:41 +0530

    store user todo in db

--- Db-mongoose/index.js
+++ Db-mongoose/index.js
@@ -86,7 +86,25 @@ app.get('/todos', auth, async (req, res) => {
 })
 
 app.post('/todo', auth, async (req, res) => {
-
+    const userId = req.userId
+    const { title, isDone } = req.body
+    const user = await UserModel.find({
+        _id: userId
+    })
+    if (user) {
+        await TodoModel.create({
+            title,
+            isDone,
+            userId
+        })
+        res.send({
+            message: 'Todo created successfully'
+        })
+    } else {
+        res.send({
+            message: 'User not found'
+        })
+    }
 })
 
 app.listen(3000)
