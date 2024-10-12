28c6ea70f1628fd8b3bc0469415042a26722c3f9
Partik Bolan <bolanpartik@gmail.com>
2024-10-12 17:29:58 +0530

    set up Mongoose connection

--- Db-mongoose/index.js
+++ Db-mongoose/index.js
@@ -2,7 +2,10 @@ const express = require('express')
 const app = express()
 const jwt = require('jsonwebtoken')
 const JWT_SECRET = 'JwtSecretHere'
-
+const { UserModel, TodoModel } = require('./db')
+const mongoose = require('mongoose')
+// Mongodb connection string
+mongoose.connect('')
 app.use(express.json())
 
 app.post('/signup', async (req, res) => {
