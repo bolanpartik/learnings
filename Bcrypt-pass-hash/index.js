a8b0ff5cc93860fb0b163aa99eb176440902c033
Partik Bolan <bolanpartik@gmail.com>
2024-10-13 22:51:53 +0530

    check hash pass and generate token on signin

--- Bcrypt-pass-hash/index.js
+++ Bcrypt-pass-hash/index.js
@@ -3,6 +3,7 @@ const app = express()
 const { UserModel, TodoModel } = require('./db')
 const mongoose = require('mongoose')
 const bcrypt = require('bcrypt')
+const jwt = require('jsonwebtoken')
 
 // MongoDB cluster connection string here
 mongoose.connect('')
@@ -34,7 +35,30 @@ app.post('/signup', async (req, res) => {
 })
 
 app.post('/signin', async (req, res) => {
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.send({
+            message: 'Please provide email and password'
+        })
+        return
+    }
+    const user = await UserModel.findOne({
+        email: email,
+    })
 
+    const matchPass = await bcrypt.compare(password, user.password)
+
+    if (matchPass) {
+        const token = jwt.sign({ userId: user._id }, JWT_SECRET)
+        res.send({
+            message: 'Sign in done',
+            token
+        })
+    } else {
+        res.send({
+            message: 'Incorrect credentials'
+        })
+    }
 })
 
 app.get('/todos', async (req, res) => {
