531b804dd0b10ed24de6a70c1704a63cec3044d9
Partik Bolan <bolanpartik@gmail.com>
2024-10-13 22:26:54 +0530

    store email and hashed pass on signup

--- Bcrypt-pass-hash/index.js
+++ Bcrypt-pass-hash/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const { UserModel, TodoModel } = require('./db')
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
 
 // MongoDB cluster connection string here
 mongoose.connect('')
@@ -9,7 +10,27 @@ mongoose.connect('')
 app.use(express.json())
 
 app.post('/signup', async (req, res) => {
-
+    const { email, password } = req.body
+    if (!email || !password) {
+        res.send({
+            message: 'Please provide email and password'
+        })
+        return
+    }
+    try {
+        const hashedPassword = await bcrypt.hash(password, 7)
+        await UserModel.create({
+            email: email,
+            password: hashedPassword
+        })
+        res.send({
+            message: 'Sign up done'
+        })
+    } catch (error) {
+        res.send({
+            message: 'User already exists'
+        })
+    }
 })
 
 app.post('/signin', async (req, res) => {
