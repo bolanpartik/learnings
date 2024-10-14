eabf6d19e51cc76dcbd24e15a0cf0cf1138dd4c2
Partik Bolan <bolanpartik@gmail.com>
2024-10-14 20:39:24 +0530

    store user info in db on signup

--- Zod-validation/index.js
+++ Zod-validation/index.js
@@ -1,6 +1,9 @@
 const express = require('express')
 const app = express()
 const mongoose = require('mongoose')
+const { UserModel, TodoModel } = require('./db')
+const bcrypt = require('bcrypt')
+const { auth, JWT_SECRET } = require('./auth')
 
 // MongoDB cluster connection string here
 mongoose.connect('')
@@ -8,6 +11,21 @@ mongoose.connect('')
 app.use(express.json())
 
 app.post('/signup', async (req, res) => {
+    const { email, password } = req.body
+    try {
+        const hashedPassword = await bcrypt.hash(password, 12)
+        await UserModel.create({
+            email: email,
+            password: hashedPassword
+        })
+        res.status(200).send({
+            message: 'Sign up done'
+        })
+    } catch (error) {
+        res.status(409).send({
+            message: 'User already exists'
+        })
+    }
 
 })
 
