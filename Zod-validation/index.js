9b961dbf281d5af93840a5639d3b5bdd4d60debd
Partik Bolan <bolanpartik@gmail.com>
2024-10-14 20:28:18 +0530

    create basic routes using express

---
+++ Zod-validation/index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+
+app.use(express.json())
+
+app.post('/signup', async (req, res) => {
+
+})
+
+app.post('/signin', async (req, res) => {
+
+})
+
+app.get('/todos', auth, async (req, res) => {
+
+})
+
+app.post('/todo', auth, async (req, res) => {
+
+})
+
+app.listen(3000)
