c0be1299c4a075ce430c854b9f2c7f9b5c8c847c
Partik Bolan <bolanpartik@gmail.com>
2024-10-20 20:18:36 +0530

    show counter conditionally

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,20 +1,42 @@
 import { useEffect, useState } from 'react'
 
 function App() {
-  const [count, setCount] = useState(0)
-  
-  useEffect(()=>{
-    setInterval(()=>{
-      setCount(count => count + 1)
-    },1000)
-  },[])
+  const [showClock, setShowClock] = useState(true)
 
+  useEffect(() => {
+    setInterval(() => {
+      setShowClock(showClock => !showClock)
+    }, 5000)
+  }, []) // empty dependency array(means runs only once when component render or mount)
 
   return (
     <>
-      <p>{count}</p>
+      {/* conditional rendering of counter */}
+      {showClock && <Counter />}
     </>
   )
+}
+
+// mount and un-mount every 5 seconds
+function Counter() {
+  const [count, setCount] = useState(0)
+
+  // runs on every mount once
+  useEffect(() => {
+    const clock = setInterval(() => {
+      console.log('inside clock')
+      setCount(count => count + 1)
+    }, 1000)
+
+    // runs when the component un-mount because empty dependency array
+    return function () {
+      clearInterval(clock)
+    }
+  }, [])
+
+  return <div>
+    {count}
+  </div>
 }
 
 export default App
