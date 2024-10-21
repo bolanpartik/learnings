82e452882c0a2d467bfc0bee155f5f18098891c1
Partik Bolan <bolanpartik@gmail.com>
2024-10-21 20:09:11 +0530

    create notification counter

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -12,11 +12,31 @@ function App() {
 
   return (
     <>
+      <NotificationCounter/>
       {/* conditional rendering of counter */}
-      {showClock && <Counter />}
+      {/* {showClock && <Counter />} */}
     </>
   )
 }
 
+
+
+function NotificationCounter() {
+  const [notifications, setNotifications] = useState(0)
+
+  function handle() {
+    setNotifications(notifications + 1)
+  }
+
+  return <div >
+    <div >
+      <img src="https://cdn-icons-png.flaticon.com/512/3239/3239958.png" alt="" />
+      <span>{notifications}</span>
+    </div>
+
+    <button onClick={handle}>Increase</button>
+
+  </div>
+}
 
 export default App
