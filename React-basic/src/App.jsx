649b77693eda3c95394e72f869d2969eec59bc89
Partik Bolan <bolanpartik@gmail.com>
2024-10-22 08:01:22 +0530

    create notification counter seperate component

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from 'react'
 import Counter from './Counter'
+import NotificationCounter from './NotificationCounter'
 
 function App() {
   const [showClock, setShowClock] = useState(true)
@@ -17,26 +18,6 @@ function App() {
       {/* {showClock && <Counter />} */}
     </>
   )
-}
-
-
-
-function NotificationCounter() {
-  const [notifications, setNotifications] = useState(0)
-
-  function handle() {
-    setNotifications(notifications + 1)
-  }
-
-  return <div >
-    <div style={{ display: 'flex', alignItems: 'flex-start' }}>
-      <img src="https://cdn-icons-png.flaticon.com/512/3239/3239958.png" alt="" style={{ width: 30 }} />
-      <span style={{ fontSize: 20,color:'red' }}>{notifications}</span>
-    </div>
-
-    <button onClick={handle} style={{marginTop:10}}>Increase</button>
-
-  </div>
 }
 
 export default App
