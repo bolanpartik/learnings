6fe9b3aa2cb7e7cdd02c3d085f3d2aa8b0b5988b
Partik Bolan <bolanpartik@gmail.com>
2024-10-23 12:09:15 +0530

    change tabs to todo

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,30 +1,33 @@
 import { useState } from 'react'
 
 function App() {
-  const [currTab, setCurrTab] = useState('home')
+  const [currTab, setCurrTab] = useState(1)
+
+  useEffect(() => {
+    console.log('sending request to backend to get data for tab ' + currTab)
+  }, [currTab])
 
   return <div>
 
     <button
-      onClick={() => setCurrTab('home')}
-      style={{ backgroundColor: currTab === 'home' ? 'skyblue' : 'inherit' }}>Home
+      onClick={() => setCurrTab(1)}
+      style={{ backgroundColor: currTab === 1 ? 'skyblue' : 'inherit' }}>todo1
     </button>
 
     <button
-      onClick={() => setCurrTab('notifications')}
-      style={{ backgroundColor: currTab === 'notifications' ? 'skyblue' : 'inherit' }}>Notifications
+      onClick={() => setCurrTab(2)}
+      style={{ backgroundColor: currTab === 2 ? 'skyblue' : 'inherit' }}>todo2
     </button>
 
     <button
-      onClick={() => setCurrTab('jobs')}
-      style={{ backgroundColor: currTab === 'jobs' ? 'skyblue' : 'inherit' }}>Jobs
+      onClick={() => setCurrTab(3)}
+      style={{ backgroundColor: currTab === 3 ? 'skyblue' : 'inherit' }}>todo3
     </button>
 
     <button
-      onClick={() => setCurrTab('contact')}
-      style={{ backgroundColor: currTab === 'contact' ? 'skyblue' : 'inherit' }}>Contact
+      onClick={() => setCurrTab(4)}
+      style={{ backgroundColor: currTab === 4 ? 'skyblue' : 'inherit' }}>todo4
     </button>
-
   </div>
 }
 
