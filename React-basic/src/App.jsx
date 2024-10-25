2c699a27ff00e7127b251df819bbb2ec7cdec469
Partik Bolan <bolanpartik@gmail.com>
2024-10-25 15:17:04 +0530

    rendering list with unique key

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,31 +1,17 @@
-import { useState } from "react";
-
 function App() {
-  return (
-    <div>
-      <Collapsible content={'Weekend Plans'}>
-        <p>Section 1: Weekend Plans</p>
-        <p>This weekend, I’m planning a trip to the mountains for some hiking. I’m excited to explore new trails and enjoy the fresh air. It’s a great way to recharge and connect with nature!</p>
-      </Collapsible>
-
-      <Collapsible content={'Book Recommendations'}>
-        <p>Section 2: Book Recommendations</p>
-        <p>I just finished reading a fantastic novel and would love to share some recommendations! If you're into thrillers, "The Silent Patient" is a must-read. What are some of your favorite books?</p>
-      </Collapsible>
-    </div>
-  );
+  const items = [
+    { id: 1, name: 'Item 1' },
+    { id: 2, name: 'Item 2' },
+    { id: 3, name: 'Item 3' }
+  ]
+  return <ListItem items={items} />
 }
 
-function Collapsible({ children, content }) {
-  const [isOpen, setOpen] = useState(false)
-
-  return (
-    <div>
-      <button onClick={() => setOpen(!isOpen)}>
-        {content} {isOpen ? ' - ' : ' + '}
-      </button>
-      {isOpen && <div>{children}</div>}
-    </div>
-  )
+function ListItem({ items }) {
+  return <ul>
+    {items.map(item => (
+      <li key={item.id}>{item.name}</li>
+    ))}
+  </ul>
 }
 export default App
