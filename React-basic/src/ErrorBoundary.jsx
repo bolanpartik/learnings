758a890c7bf06290f21452651ea76c434277c1e3
Partik Bolan <bolanpartik@gmail.com>
2024-10-26 16:59:37 +0530

    move to new file

---
+++ React-basic/src/ErrorBoundary.jsx
@@ -0,0 +1,50 @@
+import React from "react"
+
+// Usage of errorboundary 
+
+// function App() {
+//   return (
+//     <div>
+//       <ErrorBoundary>
+//         <Card1 />
+//       </ErrorBoundary>
+//       <ErrorBoundary>
+//         <Card2 />
+//       </ErrorBoundary>
+//     </div>
+//   )
+// }
+// function Card1() {
+//   return (
+//     <div>This is card one</div>
+//   )
+// }
+// function Card2() {
+//   throw new Error('Something went wrong')
+
+//   return (
+//     <div>This is card second</div>
+//   )
+// }
+
+class ErrorBoundary extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = { hasError: false }
+  }
+
+  static getDerivedStateFromError(error) {
+    return { hasError: true }
+  }
+
+  componentDidCatch(err) {
+    console.log(err)
+  }
+
+  render() {
+    if (this.state.hasError) {
+      return <div>Something went wrong</div>
+    }
+    return <div>{this.props.children}</div>
+  }
+}
