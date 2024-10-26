4ac4e2f0bbda8a981b907396680cf6f926aef432
Partik Bolan <bolanpartik@gmail.com>
2024-10-26 17:10:22 +0530

    wrap element in fragment

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,48 +1,16 @@
-import React from "react"
-
-export default function App() {
+function App() {
   return (
-    <div>
-      <ErrorBoundary>
-        <Card1 />
-      </ErrorBoundary>
-      <ErrorBoundary>
-        <Card2 />
-      </ErrorBoundary>
-    </div>
+    <>
+      <div>Hello</div>
+      <div>Hi</div>
+    </>
   )
-}
-function Card1() {
-  return (
-    <div>This is card one</div>
-  )
-}
-function Card2() {
-  throw new Error('Something went wrong')
 
-  return (
-    <div>This is card second</div>
-  )
+  // Wrong can't return two element so wrap them in fragment(<> </>)
+  // return (
+  //     <div>Hello</div>
+  //     <div>Hi</div>
+  // )
 }
 
-class ErrorBoundary extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = { hasError: false }
-  }
-
-  static getDerivedStateFromError(error) {
-    return { hasError: true }
-  }
-
-  componentDidCatch(err) {
-    console.log(err)
-  }
-
-  render() {
-    if (this.state.hasError) {
-      return <div>Something went wrong</div>
-    }
-    return <div>{this.props.children}</div>
-  }
-}
+export default App
