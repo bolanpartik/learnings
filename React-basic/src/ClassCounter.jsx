3612a6f4ad3a774c9e3aece9388ef2235142eeb3
Partik Bolan <bolanpartik@gmail.com>
2024-10-26 16:13:20 +0530

    move class counter to new file

---
+++ React-basic/src/ClassCounter.jsx
@@ -0,0 +1,16 @@
+export default class CounterClass extends Component {
+    state = {
+      count: 0
+    }
+    increment = () => {
+      this.setState({
+        count: this.state.count + 1
+      })
+    }
+    render() {
+      return <div>
+        <div>Count is : {this.state.count}</div>
+        <button onClick={this.increment}>Increment</button>
+      </div>
+    }
+  }
