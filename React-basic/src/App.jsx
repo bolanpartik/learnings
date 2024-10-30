baab9450737671f4bf3aa0433fe79bd02c328e90
Partik Bolan <bolanpartik@gmail.com>
2024-10-30 09:29:20 +0530

    move scrollRef to component

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,31 +1,10 @@
-import { useEffect, useRef, useState } from "react"
+import ScrollRef from "./ScrollRef"
+
 
 function App() {
-  const [messages, setMessages] = useState(["Heloo", "Kya haal hai"])
-  const [count, setCount] = useState(1)
-  const scrollRef = useRef()
-
-  const addMessage = () => {
-    setMessages(prevMessages => [...prevMessages, `Adding message number ${count}`]);
-    setCount(prevCount => prevCount + 1)
-  }
-
-  useEffect(() => {
-    scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
-  }, [messages])
 
   return (
-    <div>
-      <div
-        ref={scrollRef}
-        style={{ height: "100px", padding: "10px", overflowY: "scroll", border: "1px solid black" }}
-      >
-        {messages.map((msg, i) => (
-          <div key={i}>{msg}</div>
-        ))}
-      </div>
-      <button onClick={addMessage}>Add Message</button>
-    </div>
+    <ScrollRef />
   )
 }
 
