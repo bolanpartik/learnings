9c5f2852e21905f4e6d369ccf15aef5b0b8c8a03
Partik Bolan <bolanpartik@gmail.com>
2024-10-31 17:49:36 +0530

    create timer using state

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,10 +1,26 @@
-import ScrollRef from "./ScrollRef"
-
+import { useState } from "react"
 
 function App() {
+  const [timer, setTimer] = useState(0)
+  const [intervalId, setIntervalId] = useState(null)
+
+  function startClock() {
+    const id = setInterval(() => {
+      setTimer(t => t + 1)
+    }, 1000)
+    setIntervalId(id)
+  }
+  function stopClock() {
+    clearInterval(intervalId)
+    setIntervalId(null)
+  }
 
   return (
-    <ScrollRef />
+    <div>
+      <div>Timer: {timer}</div>
+      <button onClick={startClock}>Start</button>
+      <button onClick={stopClock}>Stop</button>
+    </div>
   )
 }
 
