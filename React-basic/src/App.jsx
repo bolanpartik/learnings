966a4c73a98985595857e603fc0100030032ef6d
Partik Bolan <bolanpartik@gmail.com>
2024-11-01 17:58:30 +0530

    create timer with ref

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,10 +1,25 @@
-import TimerState from "./TimerState"
+import { useRef, useState } from "react"
 
 function App() {
+  const [timer, setTimer] = useState(0)
+  const timerRef = useRef(null)
 
+  function startTimer() {
+    const id = setInterval(() => {
+      setTimer(t => t + 1)
+    }, 1000)
+    timerRef.current = id
+  }
+  function stopTimer() {
+    clearInterval(timerRef.current)
+    timerRef.current = null
+  }
   return (
     <div>
-      <TimerState />
+      <div>Timer: {timer}</div>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+
     </div>
   )
 }
