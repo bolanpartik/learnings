4925703b9cb232f641427c7d192f66f95e536f59
Partik Bolan <bolanpartik@gmail.com>
2024-11-05 08:13:37 +0530

    create a simple counter app

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,12 +1,34 @@
-import TimerRef from "./TimerRef"
+import { useState } from 'react'
 
 function App() {
+  const [count, setCount] = useState(0)
 
   return (
     <div>
-      <TimerRef />
+      <Parent count={count} setCount={setCount} />
     </div>
   )
 }
+
+function Parent({ count, setCount }) {
+  return <>
+    <Incrase count={count} setCount={setCount} />
+    <Decrease count={count} setCount={setCount} />
+    <Value count={count} setCount={setCount} />
+  </>
+}
+
+function Decrease({ count, setCount }) {
+  return <button onClick={() => setCount(count - 1)}>Decrease</button>
+}
+
+function Incrase({ count, setCount }) {
+  return <button onClick={() => setCount(count + 1)}>Increase</button>
+}
+
+function Value({ count }) {
+  return <p>Count: {count}</p>
+}
+
 
 export default App
