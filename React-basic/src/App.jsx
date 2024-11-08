ebe7df13508948dde258018abb9fe8ab549cfe5f
Partik Bolan <bolanpartik@gmail.com>
2024-11-08 22:04:01 +0530

    update the usePrev hook

--- React-basic/src/App.jsx
+++ React-basic/src/App.jsx
@@ -1,15 +1,25 @@
 import { useState } from "react"
-import { usePrev } from "./hooks/usePrev"
+import { usePrev, usePrevUpdated } from "./hooks/usePrev"
 
 function App() {
   const [count, setCount] = useState(0)
+  const [something, setSomething] = useState()
+
   const prevVal = usePrev(count)
-  
+  const prevValUpdated = usePrevUpdated(count)
+
+  // Do extra re-render
+  const glitch = ()=> {
+    setSomething(Math.random())
+  }
+
   return (
     <div>
       <p>Count: {count}</p>
-      <button onClick={()=> setCount(count + 1)}>Increase</button>
+      <button onClick={() => setCount(count + 1)}>Increase</button>
+      <button onClick={glitch}>Glitch</button>
       <p>Previous value is : {prevVal}</p>
+      <p>Updated Previous value is  : {prevValUpdated}</p>
     </div>
   )
 }
--- React-basic/src/hooks/usePrev.jsx
+++ React-basic/src/hooks/usePrev.jsx
@@ -4,10 +4,23 @@ export function usePrev(value) {
     const ref = useRef(null)
 
     // useEffect run after the function returns, so value gets updated but the function already returned with prev value
-    useEffect(() => {
+    useEffect(()=>{
         ref.current = value
-    }, [value])
+    },[value])
 
     // return first
     return ref.current
+}
+
+// slightly better approach because if we do extra render with above code then the value will get upgraded 
+export function usePrevUpdated(newVal, initial) {
+    const ref = useRef({ value: newVal, prevVal: initial })
+
+    // check if the value is updated, then run this 
+    if (ref.current.value !== newVal) {
+        ref.current.prevVal = ref.current.value
+        ref.current.value = newVal
+    }
+
+    return ref.current.prevVal
 }
