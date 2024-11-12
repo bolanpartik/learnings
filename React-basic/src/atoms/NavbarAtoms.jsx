6188e5026c970ea89a31f656d685be10b4e10c3a
Partik Bolan <bolanpartik@gmail.com>
2024-11-12 19:43:58 +0530

    use asynchronous query to get data

--- React-basic/src/atoms/NavbarAtoms.jsx
+++ React-basic/src/atoms/NavbarAtoms.jsx
@@ -1,13 +1,15 @@
 import { atom, selector } from 'recoil'
+import axios from 'axios'
 
 export const notificationsAtom = atom({
     key: 'notifications',
-    default: {
-        networkCount: 11,
-        jobCount: 3,
-        messageCount: 7,
-        notificationCount: 21
-    }
+    default: selector({
+        key: 'notificationsSelector',
+        get: async () => {
+            const response = await axios.get('https://random-data-api.com/api/v3/projects/fd622d95-9b65-43eb-8d20-fb54783dd1dc?api_key=zKUB5o_wlTxiKzWH_JSeZQ')
+            return response.data
+        }
+    })
 })
 
 export const totatNotifications = selector({
