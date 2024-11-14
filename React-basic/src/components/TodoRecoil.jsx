9c831e2d4eb24b6f4b326e4ca2c975c7e0f1f79f
Partik Bolan <bolanpartik@gmail.com>
2024-11-14 11:43:07 +0530

    use useRecoilValueLoadable for fallback

--- React-basic/src/components/TodoRecoil.jsx
+++ React-basic/src/components/TodoRecoil.jsx
@@ -1,23 +1,29 @@
-import { RecoilRoot, useRecoilValue } from "recoil";
+import { RecoilRoot, useRecoilValue, useRecoilValueLoadable } from "recoil";
 import { todoItemAtom } from "../atoms/TodoAtom";
-import { Suspense } from "react";
 
 export function TodoRecoil() {
     return <RecoilRoot>
-        <Suspense fallback={<div>Loading...</div>}>
         <Todo id={1} />
         <Todo id={1} />
         <Todo id={2} />
         <Todo id={2} />
-        </Suspense>
     </RecoilRoot>
 }
 
 function Todo({ id }) {
-    const currTodo = useRecoilValue(todoItemAtom(id))
+    const currTodo = useRecoilValueLoadable(todoItemAtom(id))
+    console.log(currTodo.state)
 
-    return <div>
-        <div>{currTodo.todo}</div>
-        <div>{currTodo.completed}</div>
-    </div>
+    if (currTodo.state === 'loading') {
+        return <div>Loading...</div>
+    }
+    else if (currTodo.state === 'hasValue') {
+        return <div>
+            <div>{currTodo.contents.todo}</div>
+            <div>{currTodo.contents.completed}</div>
+        </div>
+    }
+    else if (currTodo.state === 'hasError') {
+        return <div>Error while getting data from the database</div>
+    }
 }
