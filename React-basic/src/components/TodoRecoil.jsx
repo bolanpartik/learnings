d7173468bfdccf06bb515966ebefff1c1370777e
Partik Bolan <bolanpartik@gmail.com>
2024-11-14 11:11:57 +0530

    add suspense with fallback

--- React-basic/src/components/TodoRecoil.jsx
+++ React-basic/src/components/TodoRecoil.jsx
@@ -1,17 +1,21 @@
 import { RecoilRoot, useRecoilValue } from "recoil";
 import { todoItemAtom } from "../atoms/TodoAtom";
+import { Suspense } from "react";
 
 export function TodoRecoil() {
     return <RecoilRoot>
-        <Todo id={1}/>
-        <Todo id={1}/>
-        <Todo id={2}/>
-        <Todo id={2}/>
+        <Suspense fallback={<div>Loading...</div>}>
+        <Todo id={1} />
+        <Todo id={1} />
+        <Todo id={2} />
+        <Todo id={2} />
+        </Suspense>
     </RecoilRoot>
 }
 
 function Todo({ id }) {
     const currTodo = useRecoilValue(todoItemAtom(id))
+
     return <div>
         <div>{currTodo.todo}</div>
         <div>{currTodo.completed}</div>
